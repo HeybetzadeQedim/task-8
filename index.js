0f01b6e6d8dfba73d3f2590354a0d84569c2aa63
HeybetzadeQedim <qdimheybtzade@gmail.com>
2023-11-04 12:54:50 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,191 @@
+// let a=5
+// let b=7
+// console.log(a+b)
+
+// let bookPrice= 9
+// let bookAmount= 12
+// console.log(bookPrice * bookAmount)
+
+// var a= 7
+// var a= 9
+
+// const a=3
+// a=1
+// console.log(a+b)
+
+
+// var d=3
+// d=1
+
+// let a=
+// let b =1
+
+// var a=1
+// var a=3
+
+
+// let userName='Qedim`
+// console.log (`Salam ${userName}`)
+
+// let  fullName =  'Qedim '
+// console.log (fullName.indexOf('m'))
+
+// let a=4
+// let b=6
+// let c=9
+// let cem= a+b+c
+// console.log(cem/3)
+
+// let value1 = prompt('Birinci reqemi daxil edin')
+// let value2 = prompt('Ikinci reqemi daxil edin')
+
+// // console.log( Number(value1) + Number(value2))
+// console.log(  +value1+    (+value2)     );
+// console.log(value1-value2);
+
+// let a= ''
+// let b=String(a)
+// console.log(typeof b);
+// console.log(Boolean(a));
+
+// let c="Qedim " 
+// console.log(c[5]);
+
+// let a=12345 
+// let b=98765
+// let d = String(a*b) 
+
+
+// console.log(d[6]);
+
+
+// console.log(7/0);
+
+// const firstName= 'Qedim'
+// const lastName= 'Heybetzade'
+// const greet= `Salam, ${firstName} ${lastName} muellim;`
+// console.log(greet);
+
+
+// const firstName= "true && false"
+// console.log(25 !== "25");
+
+
+// let value = prompt('Yasinizi tez daxil edin!')
+
+// if (value>18) {
+//     console.log("Access denied");
+// }
+
+// else {
+//     console.log("Access granted");
+// }
+
+// let value = prompt('Reqem daxil edin tez')
+
+// if (value > 0) {
+//     console.log("Musbetdir");
+// }
+
+// else {
+//     console.log("Menfidir");
+// }
+    
+
+// let eded =prompt('Reqem daxil edin')
+
+// if (eded %2==0  && eded ) {
+//     console.log("Cutdur");
+// }
+
+// else {
+//     console.log("Tekdir");
+// }
+// let eded = prompt('Reqem daxil edin')
+
+// if (eded % 5==0 && eded % 3==0 ) {
+//     console.log("Menim axtardigim number budur!!");
+// }
+
+// else {
+//     console.log("Menim axtardigim number bu deyil!");
+// }
+
+// let userName = prompt('Adinizi daxil edin!')
+// let password = prompt('Parolu daxil edin!')
+
+// if (userName == "Aytac" && password == 123) {
+//     console.log("Budur!");
+// }
+// else {
+//     console.log("Bu deyil!");
+// }
+
+// let eded = prompt('Reqem daxil edin')
+
+// if (eded % 10 ==0) {
+//     console.log("ededin sonu 0 ile bitir!");
+// }
+
+// else {
+//     console.log("ededin sonu 0 ile bitmir!");
+// }
+
+// let name = prompt('Ad daxil edin!')
+
+// if (name [1] == "a") {
+//     console.log("duzdur");
+// }
+// else {
+//     console.log("sehvdir");
+// }
+
+// let userName = prompt("Ad daxil edin")
+
+// if (userName.length > 5) {
+//     console.log("Boyukdur");
+// }
+
+// else {
+//     console.log("Balacadir");
+// }
+
+// let num = prompt("Reqem daxil edin!")
+
+// if (num > 20 && num < 40) {
+//     console.log( +num + 5);
+// }
+// else {
+//     console.log(num - 7);
+// }
+
+// let num = prompt("Reqem daxil edin!")
+
+// if (num % 4 == 3) {
+//     console.log("true"); 
+// }
+// else {
+//     console.log("false");
+// }
+
+let value = confirm('Sekli silim yoxsa qalsin?')
+
+if (value = true) { 
+    console.log("Sekil silinsin!");
+}
+
+else {
+    console.log("Sekil qalsin!");
+}
+
+
+
+// let num= prompt("Birinci reqemi daxil edin!")
+// let num2= prompt("Ikinci reqemi daxil edin!")
+
+// if (num % num2 == 0) {
+//     console.log("Bolunur!");  
+// }
+// else {
+//     console.log("Bolunmur!");
+// }
